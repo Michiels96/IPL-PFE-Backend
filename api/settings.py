989afb3c8ecceddd7c5a0f4661cfe442eafeb040
Pierre Michiels <pierre.michiels@student.vinci.be>
2019-12-03 16:25:55 +0100

    heroku (7)

--- api/settings.py
+++ api/settings.py
@@ -17,6 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
+# Allow all host headers
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '[::1]']
+
+
 
 
 # Quick-start development settings - unsuitable for production
