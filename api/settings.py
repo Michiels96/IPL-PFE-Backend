e449f7a6b30ede55ddcd3648e67f6cbb8685e6ec
Pierre Michiels <pierre.michiels@student.vinci.be>
2019-12-03 16:19:58 +0100

    heroku (6)

--- api/settings.py
+++ api/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
