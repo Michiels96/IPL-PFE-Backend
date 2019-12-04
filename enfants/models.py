14b9c5c654fc6ffbbd3edec612d53e9f6313b19c
kevin <kevintang.nf@gmail.com>
2019-12-04 23:10:52 +0100

    handic

--- enfants/models.py
+++ enfants/models.py
@@ -10,7 +10,7 @@ class Enfants(models.Model):
     nom = models.CharField(max_length=50)
     prenom = models.CharField(max_length=50)
     age = models.PositiveIntegerField()
-    handicap = models.ForeignKey(Handicap, related_name='handicaps', on_delete = models.PROTECT)
+    handicap = models.ForeignKey(Handicaps, related_name='handicaps', on_delete = models.PROTECT)
 
 class HandicapEnfant(models.Model):
     handicap_enfant_id = models.AutoField(primary_key=True)
