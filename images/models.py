96d780117e576b619898b57c9af74f721fca6680
kevin <kevintang.nf@gmail.com>
2019-12-06 16:53:57 +0100

    'images'

--- images/models.py
+++ images/models.py
@@ -9,6 +9,6 @@ def upload_path(instance, filename):
 
 class Image(models.Model):
     image_id = models.AutoField(primary_key=True)
-    image = models.ImageField(blank=True, upload_to=upload_path)
+    image_url = models.ImageField(blank=True, upload_to=upload_path)
     description =  models.CharField(max_length=100)
     categorie = models.ForeignKey(Categorie, related_name='categorie',on_delete=models.PROTECT)
