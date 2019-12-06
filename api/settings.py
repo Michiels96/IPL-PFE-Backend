a3c22828b384aa92061996cd7b0c92bc533a5070
Pierre Michiels <pierre.michiels@student.vinci.be>
2019-12-06 17:28:37 +0100

    back to 849790e

--- api/settings.py
+++ api/settings.py
@@ -32,7 +32,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = '$tt)vj8sjd+a%c+(r#dc2ti&)ldh#sudjd_j%vkqv0q5)ah(qx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
