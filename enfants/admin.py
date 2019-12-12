998d49e840f2b27db5176372b5e80cbf4d59bace
kevin <kevintang.nf@gmail.com>
2019-12-12 15:13:07 +0100

    'session'

--- enfants/admin.py
+++ enfants/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-# from .models import Enfant, Handicap, HandicapEnfant,InfoSupplementaire,PersonneContact
+from .models import Enfant, Handicap, HandicapEnfant,InfoSupplementaire,PersonneContact
 
-# admin.site.register(Enfant)
-# admin.site.register(Handicap)
-# admin.site.register(HandicapEnfant)
-# admin.site.register(InfoSupplementaire)
-# admin.site.register(PersonneContact)
+admin.site.register(Enfant)
+admin.site.register(Handicap)
+admin.site.register(HandicapEnfant)
+admin.site.register(InfoSupplementaire)
+admin.site.register(PersonneContact)
